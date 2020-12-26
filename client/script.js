2bf5dd329cb81a5b3e90ab2c05b928e10c8c213e
shathaaminKhalafall <72312523+shathaaminKhalafall@users.noreply.github.com>
2020-12-26 21:52:10 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,6 +1,17 @@
+
 //edit your code 
 fetch('http://localhost:3000/user',{
     method:'GET',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+}).then(res=>res.json()).then(val=>console.log(val))
+.catch(err=>{
+    console.log(err);
+})
+
+fetch('http://localhost:3000/user',{
+    method:'post',
     headers: {
         'Content-Type': 'application/json',
       },
